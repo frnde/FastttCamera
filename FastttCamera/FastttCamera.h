3c5fdf50613be8733b87ed99d8e131fe362229ba
Christian Mittendorf <christian.mittendorf@freenet.de>
2016-05-19 13:50:08 +0200

    updated umbrella header

--- FastttCamera/FastttCamera.h
+++ FastttCamera/FastttCamera.h
@@ -9,7 +9,24 @@
 @import UIKit;
 @import AVFoundation;
 
+//! Project version number for viewer_protocol.
+FOUNDATION_EXPORT double viewer_protocolVersionNumber;
+
+//! Project version string for viewer_protocol.
+FOUNDATION_EXPORT const unsigned char viewer_protocolVersionString[];
+
+#import "UIViewController+FastttCamera.h"
+#import "IFTTTDeviceOrientation.h"
+#import "FastttCapturedImage+Process.h"
 #import "FastttCameraInterface.h"
+#import "FastttCameraTypes.h"
+#import "AVCaptureDevice+FastttCamera.h"
+#import "UIImage+FastttFilters.h"
+#import "FastttZoom.h"
+#import "UIImage+FastttCamera.h"
+#import "FastttCapturedImage.h"
+#import "FastttFocus.h"
+#import "FastttFilterCamera.h"
 
 /**
  *  Public class for you to use to create a standard FastttCamera!
